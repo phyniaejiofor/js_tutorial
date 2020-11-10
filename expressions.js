9a9a8eb6a599a349116f360d7e33ddf89c607a5d
phyniaejiofor <phyniaejiofor90@gmail.com>
2020-11-10 11:57:20 +0100

    I created a new file named expression, showing the types of expression we have

---
+++ expressions.js
@@ -0,0 +1,21 @@
+// This is bad
+// a;
+// Types of expression
+
+// 1. Variable Declaration
+// let a;
+
+// 2. Assign a value
+// a = 4;
+
+// 3. Perform an evaluation that returns a single value
+// b + c
+
+let b = 3;
+let c = 2;
+// There are three expresssions in here... can you find them
+let a = b + c;
+
+//  1. let a ... variable declaration
+//  2. perform an evalua b + c
+//  3. result assigned to a
