f10ed380474be080e317598820845d6c85af33f2
phyniaejiofor <phyniaejiofor90@gmail.com>
2020-11-10 14:59:00 +0100

    I created a new file named arrays

---
+++ arrays.js
@@ -0,0 +1,38 @@
+let a = [4, 8, 15, 16, 23, 42];
+let b = ['david', 'eddie', 'alex', 'micheal'];
+
+/*
+ console.log(a[0]);
+ console.log(a[1]);
+ console.log(a);
+
+ */
+
+//  a[0] =7;
+//  console.log(a);
+
+// object
+//  console.log(typeof a);
+
+// let c = [4, 'alex', true];
+// console.log(c);
+
+// console.log(b[4]);
+
+// console.log(a.lenght);
+// console.log(a.lenght);
+
+// a[10] = 77;
+// console.log(a);
+// console.log(a.length);
+
+
+a.push(77);
+console.log(a);
+console.log(a.length);
+
+a.pop();
+a.pop();
+a.pop();
+console.log(a);
+console.log(a.length);
