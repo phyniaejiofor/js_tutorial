3e0321cc236902f8882089f7de18f0e15ffe5220
phyniaejiofor <phyniaejiofor90@gmail.com>
2020-11-10 12:25:40 +0100

    I created a new file called operators

---
+++ operators.js
@@ -0,0 +1,36 @@
+Catergories of operators
+
+Assignment =
+Arithmetic + - * / %
+var m = 10 % 3;
+console.log(m);
+
+
+Increment / Decrement ++ --
+var a = 1;
+a++;
+console.log(a++);
+console.log(++a);
+console.log(a);
+
+
+String '' +
+
+Precedence
+var b = (1 + 2) * 3;
+console.log(b);
+
+
+Function invocation operators ()
+console.log('')
+
+
+Logical  amd : && or: ||
+
+Member accessor operators .
+console.log()
+
+Code block operators { }
+
+
+ Array element access operators []
